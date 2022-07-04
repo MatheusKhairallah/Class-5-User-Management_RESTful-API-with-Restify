3ebfcd83aad50d914e633c7639617505fc3ecd70
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-07-04 12:48:43 -0300

    Add: consuming Routes, POST, PUT and DELETE with Restify

--- client-server/routes/users.js
+++ client-server/routes/users.js
@@ -3,19 +3,60 @@ var assert = require('assert');
 var restify = require('restify-clients');
 var router = express.Router();
 
-//Creates a JSON client
-var client = restify.createJSONClient({
-  url: 'http://localhost:4000'
+var client = restify.createJsonClient({
+    url: 'http://localhost:4000'
+})
+
+router.get('/', function(req, res, next) {
+
+    client.get('/users', function(err, request, response, obj) {
+        assert.ifError(err);
+
+        res.json(obj);
+    });
+
 });
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  
-  client.get('/users', function (err, request, response, obj) {
-    assert.ifError(err);
+router.get('/:id', function(req, res, next) {
 
-    res.end(JSON.stringify(obj, null, 2));
-  });
+    client.get(`/users/${req.params.id}`, function(err, request, response, obj) {
+        assert.ifError(err);
+
+        res.json(obj);
+    });
+
+});
+
+router.put('/:id', function(req, res, next) {
+
+    client.put(`/users/${req.params.id}`, req.body, function(err, request, response, obj) {
+        assert.ifError(err);
+
+        res.json(obj);
+
+    });
+
+});
+
+router.delete('/:id', function(req, res, next) {
+
+    client.del(`/users/${req.params.id}`, function(err, request, response, obj) {
+        assert.ifError(err);
+
+        res.json(obj);
+
+    });
+
+});
+
+router.post('/', function(req, res, next) {
+
+    client.post(`/users`, req.body, function(err, request, response, obj) {
+        assert.ifError(err);
+
+        res.json(obj);
+
+    });
 
 });
 
